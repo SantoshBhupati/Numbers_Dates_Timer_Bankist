9e4919a7c588ac25d9ef110536434df721a166c4
SantoshBhupati <bhupatis20@gmail.com>
2021-12-12 17:43:58 +0530

    Maths And Rounding

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -262,3 +262,41 @@ console.log(Number.parseInt('e23')); //NaN;
 //infinite method
  console.log(Number.isFinite(20));  //true
  console.log(Number.isFinite('20')); // false
+
+ //MATH AND ROUNDING
+
+ console.log(Math.sqrt(25)); // 5
+ console.log(25 ** (1/2)); // 5
+ console.log(8 ** (1/3)); // 2
+
+ console.log(Math.max(12,222,34,67,890)); // 890
+ console.log(Math.max(12 ,34,56,'78',67)); //78 it follows type conversion
+ console.log(Math.max(12,'45px',34,10)) ; //NaN it does not follow parsinig
+
+ console.log(Math.min(12,3,67,8,90)); // 3
+ console.log(Math.PI * Number.parseFloat('5px') ** 2);  //Area of circle : 78.53981633974483
+
+ console.log(Math.random()) // it gives number between 0 to 1
+
+ const randomInt = (max,min) => Math.trunc(Math.random() * (max- min)+1) + min;
+ console.log(randomInt(5,2));
+
+ //ROUNDING INTEGERS
+ console.log(Math.trunc(23.90)); // 23
+ 
+ //round has differences
+ console.log(Math.round(23.67)); // 24
+ console.log(Math.round(23.34)); //23
+
+ //celi
+ console.log(Math.ceil(23.34)); //24
+ console.log(Math.ceil(23.90)); //24
+
+ //floor
+ console.log(Math.floor(56.12)); //56
+ console.log(Math.floor(56.90)); //56
+
+ //tofixed - always return a string
+ console.log((2.7).toFixed(0))  // 3
+ console.log((2.7).toFixed(3)) //2.700
+ console.log(+(2.745).toFixed(2)) //2.75 and its a number 
