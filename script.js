980b503999783e0a94feca44dbc65336f256019a
SantoshBhupati <bhupatis20@gmail.com>
2021-12-15 11:17:44 +0530

    SetTimeout And SetInterval

--- script.js
+++ script.js
@@ -291,13 +291,15 @@ btnLoan.addEventListener('click', function (e) {
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
+    setTimeout(function(){
     currentAccount.movements.push(amount);
   
      //Add Transfer Date
       currentAccount.movementsDates.push(new Date().toISOString());
     // Update UI
     updateUI(currentAccount);
-  }
+  },2500)
+}
   inputLoanAmount.value = '';
 });
 
@@ -471,3 +473,24 @@ console.log(Number.parseInt('e23')); //NaN;
   console.log('US',new Intl.NumberFormat('en-US',options).format(num));
   console.log('Germany',new Intl.NumberFormat('de-DE',options).format(num));
   console.log('Arebic',new Intl.NumberFormat('ar-SY',options).format(num));
+
+
+  //SETTIMEOUT AND SETINTERVAL
+  // Timers
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
